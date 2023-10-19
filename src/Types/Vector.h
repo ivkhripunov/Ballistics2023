83687c2ce492a4f7562164f7e01df7b23f0b5f2e
ivkhripunov <khripunov.iv@phystech.edu>
2023-10-19 12:08:16 +0300

    initial

---
+++ src/Types/Vector.h
@@ -0,0 +1,15 @@
+//
+// Created by ivankhripunov on 19.10.23.
+//
+
+#ifndef BALLISTICS2023_VECTOR_H
+#define BALLISTICS2023_VECTOR_H
+
+#include "Eigen/Core"
+
+namespace Ballistics {
+    template<typename T>
+    using Vector3 = Eigen::Vector3<T>;
+}
+
+#endif //BALLISTICS2023_VECTOR_H

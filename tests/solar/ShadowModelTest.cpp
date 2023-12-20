e8afff56d6b577ffafa39eabb5c74b76ddec2a72
ivkhripunov <khripunov.iv@phystech.edu>
2023-12-20 21:41:06 +0300

    подправил все про solar

--- tests/solar/ShadowModelTest.cpp
+++ tests/solar/ShadowModelTest.cpp
@@ -9,13 +9,13 @@ TEST(PENUMBRA, SET1) {
 
     const double tolerance = 1e-3;
 
-    Ballistics::Vector3d object = {0, 0, 0};
-    Ballistics::Vector3d body = {10, 0, 0};
-    Ballistics::Vector3d sun = {20, 0, 0};
+    const Ballistics::Vector3d objectPosition = {0, 0, 0};
+    const Ballistics::Vector3d bodyPosition = {10, 0, 0};
+    const Ballistics::Vector3d sunPosition = {20, 0, 0};
 
-    Ballistics::Solar::PenumbraModel penumbraModel(2, 0.5);
+    const Ballistics::Solar::PenumbraModel shadowModel(2, 0.5);
 
-    const double shadowFactor = penumbraModel.calcShadowFunction(object, sun, body);
+    const double shadowFactor = shadowModel.calcShadowFunction(objectPosition, sunPosition, bodyPosition);
     const double referenceShadowFactor = 0.75;
 
     ASSERT_NEAR(shadowFactor, referenceShadowFactor, tolerance);
@@ -25,13 +25,13 @@ TEST(PENUMBRA, SET2) {
 
     const double tolerance = 1e-15;
 
-    Ballistics::Vector3d object = {0, 0, 0};
-    Ballistics::Vector3d body = {1, 0, 0};
-    Ballistics::Vector3d sun = {2, 0, 0};
+    const Ballistics::Vector3d objectPosition = {0, 0, 0};
+    const Ballistics::Vector3d bodyPosition = {1, 0, 0};
+    const Ballistics::Vector3d sunPosition = {2, 0, 0};
 
-    Ballistics::Solar::PenumbraModel penumbraModel(1, 0.5);
+    const Ballistics::Solar::PenumbraModel shadowModel(1, 0.5);
 
-    const double shadowFactor = penumbraModel.calcShadowFunction(object, sun, body);
+    const double shadowFactor = shadowModel.calcShadowFunction(objectPosition, sunPosition, bodyPosition);
     const double referenceShadowFactor = 0;
 
     ASSERT_NEAR(shadowFactor, referenceShadowFactor, tolerance);
@@ -41,13 +41,13 @@ TEST(PENUMBRA, SET3) {
 
     const double tolerance = 1e-15;
 
-    Ballistics::Vector3d object = {1, 0, 0};
-    Ballistics::Vector3d body = {0, 0, 0};
-    Ballistics::Vector3d sun = {2, 0, 0};
+    const Ballistics::Vector3d objectPosition = {1, 0, 0};
+    const Ballistics::Vector3d bodyPosition = {0, 0, 0};
+    const Ballistics::Vector3d sunPosition = {2, 0, 0};
 
-    Ballistics::Solar::PenumbraModel penumbraModel(1, 0.5);
+    const Ballistics::Solar::PenumbraModel shadowModel(1, 0.5);
 
-    const double shadowFactor = penumbraModel.calcShadowFunction(object, sun, body);
+    const double shadowFactor = shadowModel.calcShadowFunction(objectPosition, sunPosition, bodyPosition);
     const double referenceShadowFactor = 1;
 
     ASSERT_NEAR(shadowFactor, referenceShadowFactor, tolerance);

48e894b866bef4690f48d6a4734ad49b30af1193
ivkhripunov <khripunov.iv@phystech.edu>
2023-12-20 23:01:28 +0300

    убрал юзинги в тестах дут вычислятора

--- tests/ballistics/time/DutCorrectionTest.cpp
+++ tests/ballistics/time/DutCorrectionTest.cpp
@@ -6,18 +6,10 @@
 #include <iostream>
 #include "time/DutCorrection.h"
 
-using scalar = Ballistics::scalar;
-
-template<Ballistics::TimeModule::TimeScale Scale>
-using Time = Ballistics::TimeModule::Time<Scale>;
-using DutContainer = Ballistics::TimeModule::DutContainer;
-
-using TimeScale = Ballistics::TimeModule::TimeScale;
-
 
 TEST(INTERPOLATOR, MID_POINTS) {
 
-    const Ballistics::Containers::vector<scalar> dutValues = {
+    const Ballistics::Containers::vector<Ballistics::scalar> dutValues = {
             -0.11244,
             -0.11378,
             -0.11512,
@@ -30,32 +22,32 @@ TEST(INTERPOLATOR, MID_POINTS) {
             -0.12037,
             -0.12073};
 
-    const scalar timeStartMJD_UTC = 58480;
-    const scalar timeEndMJD_UTC = 58490;
+    const Ballistics::scalar timeStartMJD_UTC = 58480;
+    const Ballistics::scalar timeEndMJD_UTC = 58490;
 
-    Ballistics::Containers::vector<scalar> timePointsMJD_UTC(dutValues.size());
+    Ballistics::Containers::vector<Ballistics::scalar> timePointsMJD_UTC(dutValues.size());
 
     for (Ballistics::indexType i = 0; i < dutValues.size(); ++i) {
-        timePointsMJD_UTC[i] = timeStartMJD_UTC + static_cast<scalar>(i);
+        timePointsMJD_UTC[i] = timeStartMJD_UTC + static_cast<Ballistics::scalar>(i);
     }
 
     ASSERT_DOUBLE_EQ(timePointsMJD_UTC[0], timeStartMJD_UTC);
     ASSERT_DOUBLE_EQ(timePointsMJD_UTC[timePointsMJD_UTC.size() - 1], timeEndMJD_UTC);
 
-    const DutContainer dutContainer(timePointsMJD_UTC, dutValues);
+    const Ballistics::TimeModule::DutContainer dutContainer(timePointsMJD_UTC, dutValues);
 
     //mid-points test
     for (Ballistics::indexType i = 0; i < dutValues.size() - 1; ++i) {
 
-        const scalar referenceInterpolatedDut = (dutValues[i] + dutValues[i + 1]) / 2;
+        const Ballistics::scalar referenceInterpolatedDut = (dutValues[i] + dutValues[i + 1]) / 2;
 
-        const scalar midPointMJD_UTC = (timePointsMJD_UTC[i] + timePointsMJD_UTC[i + 1]) / 2;
-        const Time<Ballistics::TimeModule::TimeScale::UTC_SCALE> utc = Time<Ballistics::TimeModule::TimeScale::UTC_SCALE>::buildFromMJD(
+        const Ballistics::scalar midPointMJD_UTC = (timePointsMJD_UTC[i] + timePointsMJD_UTC[i + 1]) / 2;
+        const Ballistics::TimeModule::Time<Ballistics::TimeModule::TimeScale::UTC_SCALE> utc = Ballistics::TimeModule::Time<Ballistics::TimeModule::TimeScale::UTC_SCALE>::buildFromMJD(
                 midPointMJD_UTC);
 
         ASSERT_DOUBLE_EQ(utc.mjd(), midPointMJD_UTC);
 
-        const scalar dut = dutContainer.dut(utc);
+        const Ballistics::scalar dut = dutContainer.dut(utc);
 
         ASSERT_DOUBLE_EQ(dut, referenceInterpolatedDut);
     }
@@ -63,7 +55,7 @@ TEST(INTERPOLATOR, MID_POINTS) {
 
 TEST(INTERPOLATOR, LEFT_POINTS) {
 
-    const Ballistics::Containers::vector<scalar> dutValues = {
+    const Ballistics::Containers::vector<Ballistics::scalar> dutValues = {
             -0.11244,
             -0.11378,
             -0.11512,
@@ -76,32 +68,32 @@ TEST(INTERPOLATOR, LEFT_POINTS) {
             -0.12037,
             -0.12073};
 
-    const scalar timeStartMJD_UTC = 58480;
-    const scalar timeEndMJD_UTC = 58490;
+    const Ballistics::scalar timeStartMJD_UTC = 58480;
+    const Ballistics::scalar timeEndMJD_UTC = 58490;
 
-    Ballistics::Containers::vector<scalar> timePointsMJD_UTC(dutValues.size());
+    Ballistics::Containers::vector<Ballistics::scalar> timePointsMJD_UTC(dutValues.size());
 
     for (Ballistics::indexType i = 0; i < dutValues.size(); ++i) {
-        timePointsMJD_UTC[i] = timeStartMJD_UTC + static_cast<scalar>(i);
+        timePointsMJD_UTC[i] = timeStartMJD_UTC + static_cast<Ballistics::scalar>(i);
     }
 
     ASSERT_DOUBLE_EQ(timePointsMJD_UTC[0], timeStartMJD_UTC);
     ASSERT_DOUBLE_EQ(timePointsMJD_UTC[timePointsMJD_UTC.size() - 1], timeEndMJD_UTC);
 
-    const DutContainer dutContainer(timePointsMJD_UTC, dutValues);
+    const Ballistics::TimeModule::DutContainer dutContainer(timePointsMJD_UTC, dutValues);
 
     //left-points test
     for (Ballistics::indexType i = 0; i < dutValues.size() - 1; ++i) {
 
-        const scalar referenceInterpolatedDut = dutValues[i];
+        const Ballistics::scalar referenceInterpolatedDut = dutValues[i];
 
-        const scalar midPointMJD_UTC = timePointsMJD_UTC[i];
-        const Time<Ballistics::TimeModule::TimeScale::UTC_SCALE> utc = Time<Ballistics::TimeModule::TimeScale::UTC_SCALE>::buildFromMJD(
+        const Ballistics::scalar midPointMJD_UTC = timePointsMJD_UTC[i];
+        const Ballistics::TimeModule::Time<Ballistics::TimeModule::TimeScale::UTC_SCALE> utc = Ballistics::TimeModule::Time<Ballistics::TimeModule::TimeScale::UTC_SCALE>::buildFromMJD(
                 midPointMJD_UTC);
 
         ASSERT_DOUBLE_EQ(utc.mjd(), midPointMJD_UTC);
 
-        const scalar dut = dutContainer.dut(utc);
+        const Ballistics::scalar dut = dutContainer.dut(utc);
 
         ASSERT_DOUBLE_EQ(dut, referenceInterpolatedDut);
     }
@@ -109,7 +101,7 @@ TEST(INTERPOLATOR, LEFT_POINTS) {
 
 TEST(INTERPOLATOR, RIGHT_POINTS) {
 
-    const Ballistics::Containers::vector<scalar> dutValues = {
+    const Ballistics::Containers::vector<Ballistics::scalar> dutValues = {
             -0.11244,
             -0.11378,
             -0.11512,
@@ -122,32 +114,32 @@ TEST(INTERPOLATOR, RIGHT_POINTS) {
             -0.12037,
             -0.12073};
 
-    const scalar timeStartMJD_UTC = 58480;
-    const scalar timeEndMJD_UTC = 58490;
+    const Ballistics::scalar timeStartMJD_UTC = 58480;
+    const Ballistics::scalar timeEndMJD_UTC = 58490;
 
-    Ballistics::Containers::vector<scalar> timePointsMJD_UTC(dutValues.size());
+    Ballistics::Containers::vector<Ballistics::scalar> timePointsMJD_UTC(dutValues.size());
 
     for (Ballistics::indexType i = 0; i < dutValues.size(); ++i) {
-        timePointsMJD_UTC[i] = timeStartMJD_UTC + static_cast<scalar>(i);
+        timePointsMJD_UTC[i] = timeStartMJD_UTC + static_cast<Ballistics::scalar>(i);
     }
 
     ASSERT_DOUBLE_EQ(timePointsMJD_UTC[0], timeStartMJD_UTC);
     ASSERT_DOUBLE_EQ(timePointsMJD_UTC[timePointsMJD_UTC.size() - 1], timeEndMJD_UTC);
 
-    const DutContainer dutContainer(timePointsMJD_UTC, dutValues);
+    const Ballistics::TimeModule::DutContainer dutContainer(timePointsMJD_UTC, dutValues);
 
     //right-points test
     for (Ballistics::indexType i = 0; i < dutValues.size() - 1; ++i) {
 
-        const scalar referenceInterpolatedDut = dutValues[i + 1];
+        const Ballistics::scalar referenceInterpolatedDut = dutValues[i + 1];
 
-        const scalar midPointMJD_UTC = timePointsMJD_UTC[i + 1];
-        const Time<Ballistics::TimeModule::TimeScale::UTC_SCALE> utc = Time<Ballistics::TimeModule::TimeScale::UTC_SCALE>::buildFromMJD(
+        const Ballistics::scalar midPointMJD_UTC = timePointsMJD_UTC[i + 1];
+        const Ballistics::TimeModule::Time<Ballistics::TimeModule::TimeScale::UTC_SCALE> utc = Ballistics::TimeModule::Time<Ballistics::TimeModule::TimeScale::UTC_SCALE>::buildFromMJD(
                 midPointMJD_UTC);
 
         ASSERT_DOUBLE_EQ(utc.mjd(), midPointMJD_UTC);
 
-        const scalar dut = dutContainer.dut(utc);
+        const Ballistics::scalar dut = dutContainer.dut(utc);
 
         ASSERT_DOUBLE_EQ(dut, referenceInterpolatedDut);
     }
